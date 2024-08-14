e72e1236ead3f704a46a3714fc9e65bfe3d06e6a
Sazib <mahiburislamsazib@gmail.com>
2024-08-14 22:54:22 +0600

    a

--- js/dark-light.js
+++ js/dark-light.js
@@ -1,7 +1,21 @@
-document.getElementById('bg-light').addEventListener('click', function (){
-    document.getElementById('theam').style.backgroundColor = 'white'
-})
+document.getElementById('theam').style.border = '2px solid black'
 
-document.getElementById('bg-dark').addEventListener('click', function (){
-    document.getElementById('theam').style.backgroundColor = 'black'
+document.getElementById('theam').addEventListener('click', function () {
+    if (document.getElementById('theam').innerText === 'dark') {
+        document.getElementById('body').style.backgroundColor = 'black'
+        document.getElementById('body').style.color = 'white'
+        document.getElementById('theam').style.backgroundColor = 'black'
+        document.getElementById('theam').style.color = 'white'
+        document.getElementById('theam').style.border = '2px solid white'
+        document.getElementById('theam').innerText = 'light'
+
+    }
+    else {
+        document.getElementById('body').style.backgroundColor = 'white'
+        document.getElementById('body').style.color = 'black'
+        document.getElementById('theam').style.backgroundColor = 'white'
+        document.getElementById('theam').style.color = 'black'
+        document.getElementById('theam').style.border = '2px solid black'
+        document.getElementById('theam').innerText = 'dark'
+    }
 })
